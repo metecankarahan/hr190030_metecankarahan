992085a43eb26d8594c2158b804d4bdde91c8fcb
metecankarahan <61199821+metecankarahan@users.noreply.github.com>
2021-01-22 03:23:47 +0300

    Update Constants.java

--- app/src/main/java/com/hr190030/metecan_karahan_final/util/Constants.java
+++ app/src/main/java/com/hr190030/metecan_karahan_final/util/Constants.java
@@ -5,6 +5,6 @@ public class Constants {
 
     public static String TIKLANAN_TAKIM_TASINANIN_BASLIGI = "tiklananTakim";
 
-    public  static String BASE_URL = "https://raw.githubusercontent.com/metecankarahan/hr190030metecankarahan/main/";
+    public  static String BASE_URL = "https://raw.githubusercontent.com/metecankarahan/hr190030metecankarahan/master/";
 
 }
